ab5c670fe09b48bee6f782abbf90edc6c3213fa1
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-03-28 18:07:52 +0200

    exercices Fichier et Voyageur

---
+++ src/main/java/com/bcefit/epita/session7/immutable/Main.java
@@ -0,0 +1,10 @@
+package com.bcefit.epita.session7.immutable;
+
+public class Main {
+    public static void main(String[] args) {
+        User p = new User("Stéphan", "Eveillé", 49) ;
+
+        User pCopy = new User("Stéphane", "Eveillé",49) ;
+
+    }
+}
